6a351bd76c592b8c23c0537cde9dd2efbd4ab6aa
Jdarmoni <jgdarmoni@gmail.com>
2019-06-19 14:39:44 -0400

    reformat entry to hook into Root component - initial commits for app and root

---
+++ frontend/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Route, Switch, Link } from 'react-router-dom'
+
+const App = () => {
+    return (
+        <>
+            <h1>Mockingbird</h1>
+        </>
+    )
+}
+
+export default App
--- frontend/mockingbird.jsx
+++ frontend/mockingbird.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Root from './components/root'
+import Root from './components/root'
 import { login, logout, signup } from './util/session_api_util'
 import configureStore from './store/store';
 
@@ -28,5 +28,5 @@ document.addEventListener('DOMContentLoaded', () => {
     window.signup = signup
 
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>eggs</h1>, root);
+    ReactDOM.render(<Root store={store} />, root);
 });
