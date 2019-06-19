43a12172fb3c9e2537c85fa0d58c067bd3389007
Jdarmoni <jgdarmoni@gmail.com>
2019-06-19 15:07:27 -0400

    minor insignificant spaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
       # namespace :api, defaults: {format: :json} do
-        resource :session, only: [:create, :destroy] 
+              resource :session, only: [:create, :destroy] 
 
       # end
       root "static_pages#root"
