a4bf9882ec8e6ad74f32420c8be1c2ed10d17df3
Jdarmoni <jgdarmoni@gmail.com>
2019-06-19 14:36:04 -0400

    initial commits

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,13 +1,12 @@
 import { combineReducers } from "redux";
-// import entitiesReducer from "./entities_reducer";
-// import session from "./session_reducer";
-// import errors from './errors_reducer'
+import entitiesReducer from "./entities_reducer";
+import session from "./session_reducer";
+import errors from './errors_reducer'
 
 const rootReducer = combineReducers({
-    //unexplicity explicit (bc I export defaulted them)
-    // entities: entitiesReducer,
-    // session: session,
-    // errors: errors
+    entities: entitiesReducer,
+    session: session,
+    errors: errors
 })
 
 export default rootReducer
