b3a6b1432db5d7478feb96d7c0e4b4c7e4ee5eb2
Rain Rues <rainrues@gmail.com>
2019-06-19 19:06:35 -0400

    Got map to show up

--- frontend/components/Map/map.jsx
+++ frontend/components/Map/map.jsx
@@ -1,40 +1,56 @@
 import React from 'react';
-import { mapstyle } from "./lib/assets/map-styling";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import { mapstyle } from "../../../lib/assets/map-styling";
 
-class Map extends React.Component {
+export class MapContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      center: {},
       errors: this.props.errors
     };
   }
 
-  let map;
+  // let map;
 
-  initMap = {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: { lat: 40.720310, lng: -73.913242 },
-      zoom: 11,
-      styles: mapstyle,
-      mapTypeControl: false,
-      disableDefaultUI: true,
-      zoomControl: false,
-      scaleControl: false,
-      streetViewControl: false,
-      rotateControl: false,
-      fullscreenControl: true
-    }
-  }
+  // initMap = {
+  //   map = new google.maps.Map(document.getElementById('map'), {
+  //     center: { lat: 40.720310, lng: -73.913242 },
+  //     zoom: 11,
+  //     styles: mapstyle,
+  //     mapTypeControl: false,
+  //     disableDefaultUI: true,
+  //     zoomControl: false,
+  //     scaleControl: false,
+  //     streetViewControl: false,
+  //     rotateControl: false,
+  //     fullscreenControl: true
+  //   }
+  // }
 
-componentDidMount() {
-  initMap();
-}
+// componentDidMount() {
+//   initMap();
+// }
 
   render() {
     return (
-      <div id="map">{map}</div>
+      <Map
+        google = {this.props.google}
+        zoom = {8}
+        styles = {mapstyle}
+        mapTypeControl = {false}
+        disableDefaultUI = {true}
+        zoomControl = {false}
+        scaleControl = {false}
+        streetViewControl = {false}
+        rotateControl = {false}
+        fullscreenControl = {true}
+        initialCenter = {{ lat: 47.444, lng: -122.176 }}
+      />
     )
   }
 }
 
-export default Map;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCQ1sVSKZozy7U7kxsWE2D1B6HgT9QsjH8'
+})(MapContainer)
