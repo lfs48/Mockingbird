807a2b0648f1671c8079ab9b1bef96b251569fd7
Jdarmoni <jgdarmoni@gmail.com>
2019-06-19 11:35:23 -0400

    add shares table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_19_152521) do
+ActiveRecord::Schema.define(version: 2019_06_19_152928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2019_06_19_152521) do
     t.datetime "time_end", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "shares", force: :cascade do |t|
+    t.integer "secret_id", null: false
+    t.integer "receiver_id", null: false
+    t.index ["secret_id", "receiver_id"], name: "index_shares_on_secret_id_and_receiver_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
