016add5dc26950a22867ebb9814403d61f60ef34
Jdarmoni <jgdarmoni@gmail.com>
2019-06-19 14:17:34 -0400

    add session routes, remove egg, create store and session ajaxs

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -91,19 +91,9 @@
   !*** ./frontend/components/eggs.jsx ***!
   \**************************************/
 /*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ (function(module, exports) {
 
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-
-
-var Eggs = function Eggs() {
-  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "ohhhhhhh"));
-};
-
-/* harmony default export */ __webpack_exports__["default"] = (Eggs);
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: ENOENT: no such file or directory, open '/Users/AbbeyRoad/Desktop/Mockingbird/frontend/components/eggs.jsx'");
 
 /***/ }),
 
--- frontend/components/eggs.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Eggs = ()=> {
-    return <><h1>ohhhhhhh</h1></>
-}
-
-export default Eggs
