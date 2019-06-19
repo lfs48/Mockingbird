287427ee5f794aab002044e87dbbc100897e4fb7
Jdarmoni <jgdarmoni@gmail.com>
2019-06-19 12:39:36 -0400

    configure entry file and webpack.config, add root to routes

---
+++ frontend/components/eggs.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Eggs = ()=> {
+    return <></>
+}
+
+export default Eggs
--- frontend/mockingbird.jsx
+++ frontend/mockingbird.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import Eggs from './components/eggs'
 
-// document.addEventListener('DOMContentLoaded', () => {
-//     const root = document.getElementById('root');
-//     ReactDOM.render(<h1>Welcome to Mockingbird</h1>, root);
-// });
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    ReactDOM.render(<h1>eggs!</h1> , root);
+});
