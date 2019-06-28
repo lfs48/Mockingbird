4f35a4c7e6d9e4857124a4770cea968249ea78ac
lucas schraier <lucasfschraier@gmail.com>
2019-06-27 20:05:34 -0400

    Swap line order to set root before defining routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-      namespace :api, defaults: {format: :json} do
+        
+        root to: "static_pages#root" 
+
+        namespace :api, defaults: {format: :json} do
               resource :session, only: [:create, :destroy]
               resources :users, only: [:index, :create, :show]
 
-      end
-      root "static_pages#root"
+        end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
