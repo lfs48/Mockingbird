76248322c6f99dd9346e70606beae4cabf8626f5
lucas schraier <lucasfschraier@gmail.com>
2019-07-10 23:38:26 -0400

    Import and render MainMenu component rather than container

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Map from './Map/map';
 import { Route, Switch, Link } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
-import MainMenuContainer from './main_menu/main_menu_container';
+import MainMenu from './main_menu/main_menu';
 import FormContainer from './form/form_container';
 import Splash from './splash';
 
@@ -12,7 +12,7 @@ const App = () => {
         <>
 
             <Switch>
-                <ProtectedRoute exact path='/home' component={MainMenuContainer} />
+                <ProtectedRoute exact path='/home' component={MainMenu} />
                 <AuthRoute exact path='/form' component={FormContainer} />
                 <AuthRoute exact path='/' component={Splash} />
             </Switch>
