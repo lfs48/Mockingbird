b37bf2cc68eeb0fc9e9d0e414cbb067bc0609e21
lucas schraier <lucasfschraier@gmail.com>
2019-07-18 13:40:58 -0400

    Delegate navbar to separate component

--- frontend/components/main_menu/main_menu.jsx
+++ frontend/components/main_menu/main_menu.jsx
@@ -1,36 +1,14 @@
-import React, {useState, useEffect} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {logout} from '../../actions/session_actions';
+import React from 'react';
+import Navbar from '../navbar/navbar';
 import Map from '../Map/map';
 
 const MainMenu = () => {
-
-  const dispatch = useDispatch();
-
-  const currentUser = useSelector(state => state.entities.users[state.session.id])
-
-  const handleLogout = () => {
-    dispatch(logout());
-  }
 
   return (
     <>
 
       <main className="main-menu-content-container">
-        <div className="navbar-container">
-          <div className="navbar-title">Mockingbird</div>
-          <div className="navbar-user-container">
-            <div className="navbar-username-container">
-              <span className="navbar-username">
-                {currentUser.username}
-              </span>
-            </div>
-            <div className="navbar-logout-button-container">
-              <button className="navbar-logout-button" onClick={() => handleLogout()}>Log Out</button>
-            </div>
-          </div>
-        </div>
-
+        <Navbar/>
         <div className="map-container">
           <Map/>
         </div>
